eb9eecf9c5b69b9cc81b8bf84890e0e30a7d61ac
joser andres fuentes riano <andrejoser39@gmail.com>
2026-04-19 17:32:32 -0500

    creacion de las funciones de def o per

--- c4.js
+++ c4.js
@@ -1,3 +1,12 @@
 const defaults = { tema: "claro", idioma: "es" }; 
 
-const configurarUsuario = (defaults, personalizadas) 
+const configurarUsuario = (defaults, personalizadas) => {
+  // 1. Usamos spread para combinar sin mutar. 
+  // 'personalizadas' va al final para que sobrescriba a 'defaults'
+  const configuracionFinal = { ...defaults, ...personalizadas };
+
+  // 2. Usamos destructuración para obtener el idioma
+  const { idioma } = configuracionFinal; //
+
+  return idioma;
+};
