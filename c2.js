221a33bedcbddde0f2e1bf6f17ba759774630155
joser andres fuentes riano <andrejoser39@gmail.com>
2026-04-19 16:43:12 -0500

    arreglos de estructura

---
+++ c2.js
@@ -0,0 +1,2 @@
+const inventario = ["cámara", "trípode", "micrófono"]; // Lista de inventario inicial
+const agregarInventario = (lista, nuevoItem) => { // Función para agregar un nuevo ítem al inventario
